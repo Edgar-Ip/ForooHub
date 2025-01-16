ac24bb931388719e3a6544c14e99cb3963bbb877
Edgar <hhhedgar316@gmail.com>
2025-01-16 16:01:34 -0600

    Creación de API Rest para aplicación Foro Hub

---
+++ src/main/java/com/foroohub/ForooHub/ForooHubApplication.java
@@ -0,0 +1,13 @@
+package com.foroohub.ForooHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForooHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForooHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foroohub/ForooHub/ForooHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foroohub.ForooHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForooHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
